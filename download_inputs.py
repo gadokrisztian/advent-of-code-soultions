2bf7b9161f4448658a9ab4e28b1c9e482e74236b
gadokrisztian <85160134+gadokrisztian@users.noreply.github.com>
2023-09-16 15:45:55 +0200

    problem_downloader done

--- download_inputs.py
+++ download_inputs.py
@@ -5,8 +5,6 @@ import socket
 target_host = "adventofcode.com"
 target_port = 443
 
-current_dir = Path(__file__).parent
-
 def parse_socket_response(response):
     # Split the response into header and body
     header, body = response.split(b'\r\n\r\n', 1)
@@ -24,14 +22,14 @@ def parse_socket_response(response):
     # Return the parsed components
     return http_version, int(status_code), headers, body
 
-def download_input(year: int, day: int, session_id: str, location: str):
-    if Path(location).exists():
+def download_input(year: int, day: int, session_id: str, location: Path):
+    if location.exists():
         print(f"File {location} already exists. Skipping download.")
     
     print(f"Downloading {year} {day}...", end="", flush=True)
 
     context = ssl.create_default_context()
-    with context.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_hostname=target_host) as s, open(location, "wb") as f:
+    with context.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_hostname=target_host) as s, location.open("wb") as f:
         s.connect((target_host, target_port))
         body = f"GET /2022/day/1/input HTTP/1.1\r\nHost: {target_host}\r\nConnection: close\r\nUser-Agent: input-download/1.0\r\nCookie: session={session_id}\r\n\r\n"
         s.sendall(body.encode("utf-8"))
@@ -43,7 +41,7 @@ def download_input(year: int, day: int, session_id: str, location: str):
                 break
             response.append(chunk)
 
-        http_version, status_code, headers, body = parse_socket_response(b"".join(response))
+        _, status_code, _, body = parse_socket_response(b"".join(response))
         if status_code != 200:
             raise RuntimeError(f"Failed to download input. Status code: {status_code}")
         else:
@@ -54,7 +52,20 @@ def download_input(year: int, day: int, session_id: str, location: str):
 
 
 if __name__ == "__main__":
-    token = "53616c7465645f5fcb2337c78220140dfe9c558b1e71f596a96eeb51d296cd8b9292c4fc5712926707307e3da8f7d346c67aa7ed362eefa183170f37f491d9111"
-    download_input(2022, 1, token, current_dir.joinpath("input.txt").as_posix())
-    
+    from pathlib import Path
+    from os import getenv
 
+    token = getenv("AOC_SESSION_ID")
+    if token is None:
+        token = input("Please enter your session ID: ")
+
+    for year_path in filter(Path.is_dir, Path(__file__).parent.glob("20*")):
+        year = int(year_path.name)
+        for problem_path in filter(Path.is_dir, year_path.iterdir()):
+            problem_number = int(problem_path.name)
+            input_path = problem_path.joinpath("input.txt")
+            download_input(year, problem_number, token, input_path)
+            for programming_language in filter(Path.is_dir, problem_path.iterdir()):
+                dst = programming_language.joinpath("input.txt")
+                if not dst.exists():
+                    input_path.hardlink_to(dst)
